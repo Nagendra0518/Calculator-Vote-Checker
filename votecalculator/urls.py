af52e9b4f2f49dee5bc19907bcd029bf27e658fc
Nagendra0518 <nagendrapalla2001@gmail.com>
2024-12-12 14:32:26 +0530

    vote

--- votecalculator/urls.py
+++ votecalculator/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import settings
 from django.conf.urls.static import static
 
 from django.urls import path, include
@@ -26,5 +25,3 @@ urlpatterns = [
     path('vote/',include('voteapp.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
